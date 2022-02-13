24337839984eb53a1d1606dc79145dc546724a88
Stephen Behnfeldt <stephen@behnfeldt.pro>
2022-02-13 07:33:48 -0500

    Begin replacing hard-codings with variables.
    With the intent of eventually moving these into a config file and/or environment variables and/or command-line options.

--- wp-backup.php
+++ wp-backup.php
@@ -1,7 +1,23 @@
 <?php
-require_once 'public_html/wp-config.php';
 
+$wpdir = '.';
+$backupdir = 'wp-backups';
 
-$filename = sprintf( 'wp-backups/%s-backup_%s.sql.gz', DB_NAME, date( 'Y-m-d_H-i-s') );
-$cmd = sprintf( 'mysqldump -h %s -u %s -p%s --no-tablespaces %s | gzip > %s', DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, $filename );
-@exec($cmd, $output, $result);
+$wpconfigfile = implode(DIRECTORY_SEPARATOR, [$wpdir, 'wp-config.php']);
+
+if (!file_exists($wpconfigfile)) {
+    die(sprintf('Cannot find Wordpress config file "%s"', $wpconfigfile));
+}
+require_once $wpconfigfile;
+
+if (!file_exists($backupdir)) {
+    if (!mkdir($backupdir)) {
+        die(sprintf('Cannot create backup directory "%s"', $backupdir));
+    }
+}
+
+$filename = sprintf('%s/%s-backup_%s.sql.gz', $backupdir, DB_NAME, date('Y-m-d_H-i-s'));
+$cmd = sprintf('mysqldump -h %s -u %s -p%s --no-tablespaces %s | gzip > %s', DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, $filename);
+exec($cmd, $output, $result);
+
+exit;
